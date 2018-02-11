252f0327c4ddb2c48ab3991cee03ac780ff732f9
Helen Liutongco <liutongcoh@gmail.com>
2018-02-11 14:43:52 -0600

    Added javascript code for pixel art maker

--- designs.js
+++ designs.js
@@ -1,10 +1,31 @@
-// Select color input
-// Select size input
+let colorInput = document.getElementById('colorPicker');
+let heightInput = document.getElementById('inputHeight');
+let widthInput = document.getElementById('inputWeight');
+let sizeForm = document.getElementById('sizePicker');
+let table = document.getElementById('pixelCanvas');
 
-// When size is submitted by the user, call makeGrid()
+function makeGrid(event) {
+  //deletes any pre-existing table
+  while(table.rows.length>0) {
+    table.deleteRow(0);
+  }
+  //prevents any form submit weirdness
+  event.preventDefault();
 
-function makeGrid() {
-
-// Your code goes here!
-
+  //builds the table based on input
+  for(let counter = 0; counter < heightInput.value; counter++) {
+    let row = table.insertRow(table.rows.length);
+    for(let counter = 0; counter < widthInput.value; counter++) {
+        let cell = row.insertCell(0);
+    }
+  }
 }
+
+//users color input to change the input of the clicked cell
+//the clicked cell is event.path[0]
+function colorCell(event) {
+  event.path[0].style.backgroundColor = colorInput.value;
+}
+
+sizeForm.addEventListener('submit', makeGrid);
+table.addEventListener('click', colorCell);
